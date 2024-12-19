9938ed3b111ec24e618098c91d90b96461dbe91e
vgoosakova <vgoosakova@gmail.com>
2024-12-19 22:27:56 +0200

    Fix images

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './PaymentForm.css';
 import {Tooltip} from '@mui/material';
+import arrowLeft from 'assets/arrow-left.svg';
 
 type FormData = {
   cardNumber: string;
@@ -96,7 +97,7 @@ const PaymentForm: React.FC = () => {
       {success && <div className="success-message">Payment was successful!</div>}
 
       <header className="header">
-        <img className="header__arrow" src="/assets/arrow-left.svg" alt="Arrow"/>
+        <img className="header__arrow" src="./payment-form/assets/arrow-left.svg" alt="Arrow"/>
         <p className="fw-bold header__title text_dark-grey">Checkout</p>
         <div className="text-end">
           <button className="button p-2 text_light-grey">Eng</button>
@@ -106,12 +107,12 @@ const PaymentForm: React.FC = () => {
 
       <div className="d-flex flex-column flex-lg-row gap-4 gap-lg-5 justify-content-center main-block">
         <div className="position-relative main-block__payment">
-          <img className="position-absolute main-block__arrow" src="/assets/arrow-left.svg" alt="Arrow"/>
+          <img className="position-absolute main-block__arrow" src="./payment-form/assets/arrow-left.svg" alt="Arrow"/>
           <p className="fw-bold main-block__payment-title text_dark-grey">Checkout</p>
           <h2 className="mb-0 fw-bold main-block__payment_title text_dark-grey">5 days free</h2>
           <p className="main-block__payment_subtitle text_dark-grey">then 299.99 UAH per 14 days</p>
           <button className="button main-block__apple-pay-btn w-100">
-            <img src="/assets/apple-pay-logo.svg" alt="Apple Pay"/>
+            <img src="./payment-form/assets/apple-pay-logo.svg" alt="Apple Pay"/>
           </button>
 
           <div className="d-flex justify-content-between align-items-center mb-2 mb-lg-3 gap-3">
@@ -167,7 +168,7 @@ const PaymentForm: React.FC = () => {
                 {!errors.cvc &&
                     <Tooltip title="The 3-digit code on the back of your card" placement="top">
                         <button className="button position-absolute form-group__info-btn">
-                            <img src="/assets/info.svg" alt="Info"/>
+                            <img src="./payment-form/assets/info.svg" alt="Info"/>
                         </button>
                     </Tooltip>
                 }
@@ -211,7 +212,7 @@ const PaymentForm: React.FC = () => {
 
       <footer className="d-flex justify-content-center mt-3">
         <p className="mb-0 me-1 footer__text text_grey">Powered by</p>
-        <img src="/assets/solid-logo.svg" alt="Apple Pay"/>
+        <img src="./payment-form/assets/solid-logo.svg" alt="Apple Pay"/>
       </footer>
     </div>
   );
